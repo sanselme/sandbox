e41a44ab055219e4f46fdaf1f6510b27bcec9017
Schubert Anselme <schubert@anselm.es>
2022-10-10 13:53:20 -0400

    Generics 8
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -1,18 +1,18 @@
 package main
 
-import "fmt"
+func Compare[T int | bool](a, b T) int {
+  // a == b -> 0
+  if a == b {
+    return 0
+  }
 
-type Id int
+  // a < b -> -1
+  if a < b {
+    return -1
+  }
 
-func Has[T ~int](list []T, value T) bool {
-	for _, v := range list {
-		if v == value {
-			return true
-		}
-	}
-	return false
+  return 1
 }
 
 func main() {
-	fmt.Printf("Has 2 %v\n", Has([]Id{1, 2}, 2))
 }
