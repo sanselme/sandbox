de5d143fea0b46e5282030b3daa4489df1ad95b1
Schubert Anselme <schubert@anselm.es>
2022-10-10 12:41:13 -0400

    Generics 2
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -2,23 +2,10 @@ package main
 
 import "fmt"
 
-func Has[T comparable](list []T, value T) bool {
-  for _, v := range list {
-    if v == value {
-      return true
-    }
-  }
-  return false
-}
-
-func NewEmptyList[T any]() []T {
-  return make([]T, 0)
+func PrintThings[A, B any, C ~int](a1, a2 A, b B, c C) {
+	fmt.Printf("%v %v %v %v\n", a1, a2, b, c)
 }
 
 func main() {
-  fmt.Printf("Has: a %v\n", Has([]string{"a", "b"}, "a"))
-  fmt.Printf("Has: c %v\n", Has([]string{"a", "b"}, "c"))
-  fmt.Printf("Has: 2 %v\n", Has([]int{1, 2, 3}, 2))
-
-  fmt.Printf("MyList: %v\n", NewEmptyList[int]())
+	PrintThings(1, 2, "three", 4)
 }
