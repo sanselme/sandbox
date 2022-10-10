26e702e7c49d69e7501f7cea821f0b4a66a11c19
Schubert Anselme <schubert@anselm.es>
2022-10-10 14:04:43 -0400

    Generics End
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -1,18 +1,31 @@
 package main
 
-func Compare[T int | bool](a, b T) int {
-  // a == b -> 0
-  if a == b {
-    return 0
-  }
+import (
+	"fmt"
+	"math"
+)
 
-  // a < b -> -1
-  if a < b {
-    return -1
-  }
+type Currency interface {
+	~int | ~int64
+	ISO4127Code() string
+	Decimal() int
+}
 
-  return 1
+type CAD int64
+
+func (c CAD) ISO4127Code() string {
+	return "CAD"
+}
+
+func (c CAD) Decimal() int {
+	return 2
+}
+
+func PrintBalance[T Currency](b T) {
+	balance := float64(b) / math.Pow10(b.Decimal())
+	fmt.Printf("%.*f %s\n", b.Decimal(), balance, b.ISO4127Code())
 }
 
 func main() {
+	PrintBalance(CAD(250))
 }
