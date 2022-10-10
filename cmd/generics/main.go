39587eaa3a8ed86725a20bbb78db04d4354efab7
Schubert Anselme <schubert@anselm.es>
2022-10-10 13:45:37 -0400

    Generics 5
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- cmd/generics/main.go
+++ cmd/generics/main.go
@@ -2,17 +2,15 @@ package main
 
 import "fmt"
 
-func Has[T any](list []T, value T, equal func(a, b T) bool) bool {
-  for _, v := range list {
-    if equal(v, value) {
-      return true
-    }
-  }
-  return false
+func Has[T comparable](list []T, value T) bool {
+	for _, v := range list {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-  equalInt := func(a, b int) bool { return a == b }
-
-  fmt.Printf("Has 2 %v\n", Has([]int{1, 2}, 2, equalInt))
+	fmt.Printf("Has 2 %v\n", Has([]int{1, 2}, 2))
 }
