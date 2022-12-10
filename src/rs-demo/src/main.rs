7c92fe960fde6c0ea6948c60f87e6c81c0bcfca3
Schubert Anselme <schubert@anselm.es>
2022-12-10 17:53:52 -0500

    rust trait bound
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- src/rs-demo/src/main.rs
+++ src/rs-demo/src/main.rs
@@ -1,40 +1,12 @@
-trait Description {
-    fn describe(&self) -> String {
-        String::from("an object flying through space")
-    }
+use std::{any, fmt};
+
+fn print_type<T: fmt::Debug>(item: T) {
+    println!("{:?} is {}", item, any::type_name::<T>());
 }
 
-struct Satellite {
-    name: String,
-    velocity: f64, // miles per second
-}
-
-impl Description for Satellite {}
-
-struct SpaceStation {
-    name: String,
-    crew_size: u8,
-    altitude: u32, // miles
-}
-
-impl Description for SpaceStation {
-    fn describe(&self) -> String {
-        format!(
-            "the {} flying at {} miles height with {} crew members on board!",
-            self.name, self.altitude, self.crew_size
-        )
-    }
-}
 fn main() {
-    let hubble = Satellite {
-        name: String::from("Hubble Telescope"),
-        velocity: 4.72,
-    };
-    let iss = SpaceStation {
-        name: String::from("International Space Station"),
-        crew_size: 6,
-        altitude: 254,
-    };
-    println!("hubble is {}", hubble.describe());
-    println!("iss is {}", iss.describe())
+    print_type(13);
+    print_type(13.0);
+    print_type("thirteen");
+    print_type([13]);
 }
