65c056ffbf45276e38fb327c53625b6be2c9cdc6
Schubert Anselme <schubert@anselm.es>
2022-12-10 17:09:42 -0500

    rust generics
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- src/rs-demo/src/main.rs
+++ src/rs-demo/src/main.rs
@@ -1,29 +1,27 @@
-struct Rectangle {
-    width: f64,
-    height: f64,
+#[derive(Debug)]
+struct Rectangle<T, U> {
+    width: T,
+    height: U,
 }
 
-impl Rectangle {
-    fn new(width: f64, height: f64) -> Rectangle {
-        Rectangle { width, height }
-    }
-
-    fn get_area(&self) -> f64 {
-        self.width * self.height
-    }
-
-    fn scale(&mut self, scalar: f64) {
-        self.height *= scalar;
-        self.width *= scalar;
+impl<T, U> Rectangle<T, U> {
+    fn get_width(&self) -> &T {
+        &self.width
     }
 }
 
+impl Rectangle<u8, u8> {
+    fn get_perimeter(&self) -> u8 {
+        2 * self.width + 2 * self.height
+    }
+}
 fn main() {
-    let mut rect = Rectangle::new(1.2, 3.4);
-    assert_eq!(rect.get_area(), 4.08);
+    let rect = Rectangle {
+        width: 1u8,
+        height: 3u8,
+    };
 
-    rect.scale(0.5);
-    assert_eq!(rect.get_area(), 1.02);
-
-    println!("Tests passed!");
+    println!("rect is {:?}", rect);
+    println!("width is {:}", rect.get_width());
+    println!("perimeter is {}", rect.get_perimeter());
 }
