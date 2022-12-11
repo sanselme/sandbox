41a9931b19bc39faf1c6b747866f5d14f1c6a66e
Schubert Anselme <schubert@anselm.es>
2022-12-11 14:45:48 -0500

    rust ex 14.09
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- src/rs-demo/src/main.rs
+++ src/rs-demo/src/main.rs
@@ -1,17 +1,31 @@
-#[derive(Debug)]
-enum Shape {
-    Circle(f64),             // radius
-    Rectangle(f64, f64),     // width, height
-    Triangle(f64, f64, f64), // sides a, b, c
+use std::fmt;
+
+type Lat = f64;
+type Long = f64;
+
+enum Location {
+    Anonymous,
+    Known(Lat, Long),
+    Unknown,
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Anonymous => write!(f, "anonymous"),
+            Self::Unknown => write!(f, "unknown"),
+            Self::Known(lat, long) => write!(f, "{} lat / {} long", lat, long),
+        }
+    }
 }
 
 fn main() {
-    let my_shape = Shape::Rectangle(1.2, 3.4);
-    println!("my shape is {:?}", my_shape);
+    let address = Location::Unknown;
+    println!("Location: {}", address);
 
-    match my_shape {
-        Shape::Circle(r) => println!("Circle with radius {}", r),
-        Shape::Rectangle(w, h) => println!("{} x {} Rectangle", w, h),
-        Shape::Triangle(a, b, c) => println!("Triangle with sides {}, {}, {}", a, b, c),
-    }
+    let address = Location::Anonymous;
+    println!("Location: {}", address);
+
+    let address = Location::Known(28.608295, -80.604177);
+    println!("Location: {}", address);
 }
