77042fdc2081984ffdee8c417b373342554f1dc4
Schubert Anselme <schubert@anselm.es>
2022-12-11 15:12:40 -0500

    rust return error
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- src/rs-demo/src/main.rs
+++ src/rs-demo/src/main.rs
@@ -1,14 +1,21 @@
 use std::{fs, io};
 
-fn main() {
-    let result = fs::read_to_string("the_ultimate_question.txt");
-    let contents = match result {
-        Ok(message) => message,
-        Err(error) => match error.kind() {
-            io::ErrorKind::NotFound => String::from("File not found"),
-            io::ErrorKind::PermissionDenied => String::from("Permission denied"),
-            _ => panic!("Another type of error: {}", error),
-        },
+fn read_and_combine(f1: &str, f2: &str) -> Result<String, io::Error> {
+    let mut s1 = fs::read_to_string(f1)?;
+    let s2 = match fs::read_to_string(f2) {
+        Ok(s) => s,
+        Err(e) => return Err(e),
     };
-    println!("contents is: {}", contents);
+
+    s1.push('\n');
+    s1.push_str(&s2);
+    Ok(s1)
+}
+
+fn main() {
+    let result = read_and_combine("planets.txt", "dwarf_planets.txt");
+    match result {
+        Ok(s) => println!("result is ...\n{}", s),
+        Err(e) => println!("there was an error: {}", e),
+    }
 }
