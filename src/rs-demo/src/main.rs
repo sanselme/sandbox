cb5e5db9933d4dcfe89036ae253d0fcc77129e42
Schubert Anselme <schubert@anselm.es>
2022-12-11 13:38:30 -0500

    rust struct lifetime
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- src/rs-demo/src/main.rs
+++ src/rs-demo/src/main.rs
@@ -1,17 +1,16 @@
-fn best_fuel<'a, 'b>(x: &'a str, y: &'b str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        x
+struct Shuttle<'a> {
+    name: &'a str,
+}
+
+impl<'a, 'b> Shuttle<'a> {
+    fn send_transmission(&'a self, msg: &'b str) -> &'b str {
+        println!("Transmission message: {}", msg);
+        msg
     }
 }
 
 fn main() {
-    let result;
-    let propellant1 = String::from("RP-1");
-    {
-        let propellant2 = String::from("LNG");
-        result = best_fuel(&propellant1, &propellant2);
-    }
-    println!("result is {}", result);
+    let vehicle = Shuttle { name: "Endeavour" };
+    let sender = vehicle.send_transmission("Greetings from orbit!");
+    println!("sender is {}", sender);
 }
