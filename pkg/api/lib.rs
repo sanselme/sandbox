7aeede648c0026502f324bf937af6322eae3f854
Schubert Anselme <schubert@anselm.es>
2024-08-23 21:26:50 +0000

    [api] Initial api
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ pkg/api/lib.rs
@@ -0,0 +1,16 @@
+// @generated
+pub mod api {
+    // @@protoc_insertion_point(attribute:api.v1alpha1)
+    pub mod v1alpha1 {
+        include!("api.v1alpha1.rs");
+        // @@protoc_insertion_point(api.v1alpha1)
+    }
+
+    // @@protoc_insertion_point(attribute:api.v1)
+    pub mod v1 {
+        include!("api.v1.rs");
+        // @@protoc_insertion_point(api.v1)
+    }
+}
+
+pub use api::*;
