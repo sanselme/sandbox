800ed9d2c5adaf9f69a55b008f9a215680f624c4
Schubert Anselme <schubert@anselm.es>
2024-11-11 22:27:34 -0500

    feat: Implement gRPC greeter service with health check and CloudEvent support
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- pkg/api/Package.swift
+++ pkg/api/Package.swift
@@ -6,22 +6,32 @@ import PackageDescription
 let package = Package(
     name: "api",
     products: [
-        .library(name: "api", targets: ["api"]),
+        // .library(name: "v1alpha1", targets: ["v1alpha1"]),
+        .library(name: "v1", targets: ["v1"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.27.0"),
         .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0-alpha.1"),
         .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0-alpha.1"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0-alpha.1"),
     ],
     targets: [
+        // .target(
+        //     name: "v1alpha1",
+        //     dependencies: [
+        //         .product(name: "GRPCCore", package: "grpc-swift"),
+        //         .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
+        //         .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+        //     ],
+        //     path: "v1alpha1"
+        // ),
         .target(
-            name: "api",
-            path: ".",
+            name: "v1",
             dependencies: [
+                .product(name: "GRPCCore", package: "grpc-swift"),
                 .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
                 .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ]
+            ],
+            path: "v1"
         ),
     ]
 )
---
+++ pkg/hello/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "hello",
+    platforms: [.macOS("15.0")],
+    products: [
+      .executable(name: "hellod", targets: ["server"]),
+      .executable(name: "hellocli", targets: ["client"]),
+    ],
+    dependencies: [
+      .package(path: "../api"),
+      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "server",
+            dependencies: [
+              .product(name: "v1", package: "api"),
+              .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+        .executableTarget(
+            name: "client",
+            dependencies: [
+              .product(name: "v1", package: "api"),
+              .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+    ]
+)
