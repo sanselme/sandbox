de68ca406949f94abe521e191fcaf8a62e7b839a
Schubert Anselme <schubert@anselm.es>
2024-11-13 04:22:05 -0500

    fix: Package.swift
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- pkg/hello/Package.swift
+++ pkg/hello/Package.swift
@@ -4,30 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello",
-    platforms: [.macOS("15.0")],
-    products: [
-      .executable(name: "hellod", targets: ["server"]),
-      .executable(name: "hellocli", targets: ["client"]),
-    ],
-    dependencies: [
-      .package(path: "../api"),
-      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "server",
-            dependencies: [
-              .product(name: "v1", package: "api"),
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-        .executableTarget(
-            name: "client",
-            dependencies: [
-              .product(name: "v1", package: "api"),
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-    ]
+  name: "hello",
+  platforms: [.macOS("15.0")],
+  products: [
+    .executable(name: "hellod", targets: ["server"]),
+    .executable(name: "hellocli", targets: ["client"]),
+  ],
+  dependencies: [
+    .package(path: "../api"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "server",
+      dependencies: [
+        .product(name: "v1", package: "api"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]
+    ),
+    .executableTarget(
+      name: "client",
+      dependencies: [
+        .product(name: "v1", package: "api"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]
+    ),
+  ]
 )
