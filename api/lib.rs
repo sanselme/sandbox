fee3ceb62c59761576b016b576a03f89f60d5e0c
Schubert Anselme <schubert@anselm.es>
2024-12-11 00:16:57 -0500

    feature: Add .gitignore and initial API module implementation
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ api/lib.rs
@@ -0,0 +1,8 @@
+// @generated
+pub mod api {
+    // @@protoc_insertion_point(attribute:api.v1)
+    pub mod v1 {
+        include!("api.v1.rs");
+        // @@protoc_insertion_point(api.v1)
+    }
+}
