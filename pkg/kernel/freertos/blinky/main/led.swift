3ee7ea29fc6c1b981fc57fc9881bb8e1b5836e7d
Schubert Anselme <schubert@anselm.es>
2024-12-11 00:15:47 -0500

    feature: Add initial implementation of FreeRTOS blinky example and utility functions
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- pkg/kernel/freertos/blinky/main/led.swift
+++ pkg/kernel/freertos/blinky/main/led.swift
@@ -7,20 +7,19 @@
 //
 
 // A simple "overlay" to provide nicer APIs in Swift
-struct Led {
+struct LED {
   var ledPin: gpio_num_t
+
   init(gpioPin: Int) {
     ledPin = gpio_num_t(Int32(gpioPin))
 
-    guard gpio_reset_pin(ledPin) == ESP_OK else {
-      fatalError("cannot reset led")
-    }
-
+    guard gpio_reset_pin(ledPin) == ESP_OK else { fatalError("cannot reset led") }
     guard gpio_set_direction(ledPin, GPIO_MODE_OUTPUT) == ESP_OK else {
       fatalError("cannot reset led")
     }
   }
-  func setLed(value: Bool) {
+
+  func setLED(value: Bool) {
     let level: UInt32 = value ? 1 : 0
     gpio_set_level(ledPin, level)
   }
--- pkg/kernel/freertos/blinky/main/main.swift
+++ pkg/kernel/freertos/blinky/main/main.swift
@@ -6,18 +6,18 @@
 //  Created by Schubert Anselme on 2024-11-11.
 //
 
-//The code will blink an LED on GPIO8. To change the pin, modify Led(gpioPin: 8)
+// The code will blink an LED on GPIO8. To change the pin, modify LED(gpioPin: 8)
 @_cdecl("app_main")
 func app_main() {
   print("Hello from Swift on ESP32-C6!")
 
   var ledValue: Bool = false
-  let blinkDelayMs: UInt32 = 500
-  let led = Led(gpioPin: 8)
+  let blinkDelayMS: UInt32 = 500
+  let led = LED(gpioPin: 8)
 
   while true {
-    led.setLed(value: ledValue)
+    led.setLED(value: ledValue)
     ledValue.toggle()  // Toggle the boolean value
-    vTaskDelay(blinkDelayMs / (1000 / UInt32(configTICK_RATE_HZ)))
+    vTaskDelay(blinkDelayMS / (1000 / UInt32(configTICK_RATE_HZ)))
   }
 }
