720dbdc418ae82d7c99daec0a3f6ef652bf14d68
Schubert Anselme <schubert@anselm.es>
2024-12-11 00:16:30 -0500

    feature: Add .gitignore files, utility functions, and initial FreeRTOS smart light implementation
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ pkg/kernel/freertos/smartlight/Matter/MatterInterface.cpp
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+#include "BridgingHeader.h"
+
+esp_err_t esp_matter::attribute::set_callback_shim(callback_t_shim callback)
+{
+  return set_callback((callback_t)callback);
+}
+
+esp_matter::cluster_t *esp_matter::cluster::get_shim(esp_matter::endpoint_t *endpoint, unsigned int cluster_id)
+{
+  return get(endpoint, (uint32_t)cluster_id);
+}
+
+esp_matter::attribute_t *esp_matter::attribute::get_shim(esp_matter::cluster_t *cluster, unsigned int attribute_id)
+{
+  return get(cluster, (uint32_t)attribute_id);
+}
+
+void recomissionFabric()
+{
+  if (chip::Server::GetInstance().GetFabricTable().FabricCount() == 0)
+  {
+    chip::CommissioningWindowManager &commissionMgr = chip::Server::GetInstance().GetCommissioningWindowManager();
+    constexpr auto kTimeoutSeconds = chip::System::Clock::Seconds16(300);
+    if (!commissionMgr.IsCommissioningWindowOpen())
+    {
+      commissionMgr.OpenBasicCommissioningWindow(kTimeoutSeconds, chip::CommissioningWindowAdvertisement::kDnssdOnly);
+    }
+  }
+}
