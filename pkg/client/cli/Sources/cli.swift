69ba958610378ad8a6b642c7c30899353b1c6c6b
Schubert Anselme <schubert@anselm.es>
2024-12-12 14:23:57 -0500

    feature: Initial hello cli
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- pkg/client/cli/Sources/cli.swift
+++ pkg/client/cli/Sources/cli.swift
@@ -7,38 +7,57 @@
 //
 
 import ArgumentParser
-import GRPCNIOTransportHTTP2
+import AsyncHTTPClient
+import Foundation
+import NIOCore
 import v1
 
-// fixme: replace with api instead of grpc api (which is for services)
 @main
 struct Greet: AsyncParsableCommand {
   static let configuration = CommandConfiguration(abstract: "Send a request to the greeter server")
 
+  @Option(help: "The address to connect to")
+  var address: String = "127.0.0.1"
+
   @Option(help: "The port to listen on")
-  var port: Int = 8080
+  var port: Int = 80
 
   @Option(help: "The person to greet")
   var name: String = ""
 
   func run() async throws {
-    try await withThrowingDiscardingTaskGroup { group in
-      let client = GRPCClient(
-        transport: try .http2NIOPosix(
-          target: .ipv4(host: "127.0.0.1", port: self.port),
-          config: .defaults(transportSecurity: .plaintext)))
+    let client = HTTPClient(eventLoopGroupProvider: .singleton)
 
-      group.addTask {
-        try await client.run()
+    do {
+      var request = HTTPClientRequest(
+        url: "http://\(self.address):\(self.port)/api/v1/sandbox/hello")
+      request.method = .POST
+      request.headers.add(name: "User-Agent", value: "Swift AsyncHTTPClient")
+      request.headers.add(name: "Content-Type", value: "application/json")
+
+      var greet = v1.Api_V1_HelloRequest()
+      greet.name = self.name
+
+      let data: Data = try greet.jsonUTF8Data()
+      request.body = .bytes(data)
+
+      let response = try await client.execute(request, timeout: .seconds(5))
+      if response.status == .ok {
+        if let contentType = response.headers.first(name: "Content-Type"),
+          contentType.contains("application/json")
+        {
+          let buffer = try await response.body.collect(upTo: 1024 * 1024)
+          let data = buffer.withUnsafeReadableBytes { Data($0) }
+          let output = try v1.Api_V1_HelloReply(jsonUTF8Data: data)
+          print(output.message)
+        }
+      } else {
+        print("invalid status code: \(response.status)")
       }
-
-      defer {
-        client.beginGracefulShutdown()
-      }
-
-      let greeter = v1.Api_V1_Greeter_Client(wrapping: client)
-      let reply = try await greeter.sayHello(.with { $0.name = self.name })
-      print(reply.message)
+    } catch {
+      print("\(error)")
     }
+
+    try await client.shutdown()
   }
 }
