c13bdf78b90cabe4d69b273748472ecfb08cc563
Schubert Anselme <schubert@anselm.es>
2024-12-13 08:17:59 -0500

    fix: Update hellocli deps
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- pkg/client/cli/Package.swift
+++ pkg/client/cli/Package.swift
@@ -7,19 +7,21 @@ let package = Package(
   name: "hellocli",
   platforms: [.macOS("15.0")],
   products: [
-    .executable(name: "hellocli", targets: ["client"]),
+    .executable(name: "hellocli", targets: ["client"])
   ],
   dependencies: [
     .package(path: "../../../api"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.24.0"),
   ],
   targets: [
     .executableTarget(
       name: "client",
       dependencies: [
-        .product(name: "v1", package: "api"),
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "AsyncHTTPClient", package: "async-http-client"),
+        .product(name: "v1", package: "api"),
       ]
-    ),
+    )
   ]
 )
